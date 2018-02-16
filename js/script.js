05e7515830f324cb07f6984b8d9f1698e3c0adcf
Eslam <eslam98.2013@gmail.com>
2018-02-16 16:47:54 +0200

    added script for slider and time stamp

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ var prev = document.getElementById("previous");
 var play = document.getElementById("play"); 
 var pause = document.getElementById("pause"); 
 var next = document.getElementById("next"); 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+
+function setup() {
+	slider.max = player.duration;
+}
 
 function playAudio() { 
     player.play(); 
@@ -14,4 +20,27 @@ function pauseAudio() {
     player.pause(); 
     pause.style.display = "none";
     play.style.display = "initial";
+}
+
+output.innerHTML = slider.value; // Display the default slider value
+
+player.ontimeupdate = function() {
+	var currTime = parseInt(this.currentTime);
+	var currMin = 0;
+	var currSec = 0;
+	if (currTime>60){
+		currMin = parseInt(currTime/60);
+		currSec = currTime-(currMin*60);
+	}
+	else {
+		currSec = currTime
+	}
+	output.innerHTML = currMin + ":" + currSec;
+	slider.value = currTime;
+}
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    player.currentTime = this.value;
 }
