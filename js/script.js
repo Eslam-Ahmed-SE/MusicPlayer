184f412af290a7269cfed3312018877cca478628
Eslam <eslam98.2013@gmail.com>
2018-02-16 11:38:02 +0200

    hide pause buttons when pause and show play, and vise versa

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-var x = document.getElementById("player"); 
+var player = document.getElementById("player"); 
+var prev = document.getElementById("previous"); 
+var play = document.getElementById("play"); 
+var pause = document.getElementById("pause"); 
+var next = document.getElementById("next"); 
 
 function playAudio() { 
-    x.play(); 
+    player.play(); 
+    play.style.display = "none";
+    pause.style.display = "initial";
 } 
 
 function pauseAudio() { 
-    x.pause(); 
+    player.pause(); 
+    pause.style.display = "none";
+    play.style.display = "initial";
 }
