3f631767a63f46aa9acd21868939cd0f44f7f133
Eslam <eslam98.2013@gmail.com>
2018-02-16 17:28:56 +0200

    added remaining time (html, js)

--- js/script.js
+++ js/script.js
@@ -7,9 +7,28 @@ var slider = document.getElementById("nowPlayingSlider");
 var output = document.getElementById("timeStamp");
 var volumeSlider = document.getElementById("volumeSlider");
 var volume = document.getElementById("volume");
+var fullTime = document.getElementById("fullTime");
+var remainingOut = document.getElementById("remaining");
+
+
+
+
 
 function setup() {
 	slider.max = player.duration;
+
+	var duration = parseInt(player.duration);
+	var durationMin = 0;
+	var durationSec = 0;
+
+	if (duration>60){
+		durationMin = parseInt(duration/60);
+		durationSec = duration-(durationMin*60);
+	}
+	else {
+		durationSec = duration;
+	}
+	fullTime.innerHTML = " / " + durationMin + ":" + durationSec; // Display the default slider value
 }
 
 function playAudio() { 
@@ -25,6 +44,7 @@ function pauseAudio() {
 }
 
 
+
 //---------time slider------------
 output.innerHTML = slider.value; // Display the default slider value
 
@@ -32,20 +52,35 @@ player.ontimeupdate = function() {
 	var currTime = parseInt(this.currentTime);
 	var currMin = 0;
 	var currSec = 0;
+
+	var remaining = parseInt(player.duration) - currTime;
+	var remainingMin = 0;
+	var remainingSec = 0;
+
 	if (currTime>60){
 		currMin = parseInt(currTime/60);
 		currSec = currTime-(currMin*60);
 	}
 	else {
-		currSec = currTime
+		currSec = currTime;
 	}
+	//-----------------------
+	if (remaining>60){
+		remainingMin = parseInt(remaining/60);
+		remainingSec = remaining-(remainingMin*60);
+	}
+	else {
+		remainingSec = remaining;
+	}
+
 	output.innerHTML = currMin + ":" + currSec;
+	remainingOut.innerHTML = "Remaining: -" + remainingMin + ":" + remainingSec;
 	slider.value = currTime;
 }
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-    output.innerHTML = this.value;
+    output.innerHTML = this.value ;
     player.currentTime = this.value;
 }
 //----------------------------------
@@ -59,3 +94,6 @@ volumeSlider.oninput = function() {
     player.volume = this.value/100;
 }
 //----------------------------------
+
+
+
