e841c2f50a64e1f91a1c68b2f419727005420391
Eslam <eslam98.2013@gmail.com>
2018-02-16 20:12:06 +0200

    added playlist table show function

--- js/script.js
+++ js/script.js
@@ -9,12 +9,28 @@ var volumeSlider = document.getElementById("volumeSlider");
 var volume = document.getElementById("volume");
 var fullTime = document.getElementById("fullTime");
 var remainingOut = document.getElementById("remaining");
+var playlistTable = document.getElementById("playlistTable");
+var songs = [[],[],[],[],[],[]] 
+songs[0][0] = 'music/t1.mp3';
+songs[0][1] = 'Thunder - Imagin Dragons';
+songs[1][0] = 'music/t2.mp3';
+songs[1][1] = 'Havana - Camilia Cabilo';
+songs[2][0] = 'music/t3.mp3';
+songs[2][1] = 'I Want You to Know (Ft. Selena Gomez) - Zedd';
+songs[3][0] = 'music/t4.mp3';
+songs[3][1] = 'Stay - Zedd & Alessia Cara';
+songs[4][0] = 'music/t5.mp3';
+songs[4][1] = 'Wolves - Selena Gomez & Marshmello';
+songs[5][0] = 'music/t6.mp3';
+songs[5][1] = 'It Aint Me - Selena Gomez & Kygo';
+var indexR = 0;
+var indexC = 0;
 
-
-
+player.src = songs[indexR][indexC];
 
 
 function setup() {
+	showPlaylist();
 	slider.max = player.duration;
 
 	var duration = parseInt(player.duration);
@@ -31,6 +47,23 @@ function setup() {
 	fullTime.innerHTML = " / " + durationMin + ":" + durationSec; // Display the default slider value
 }
 
+function showPlaylist(){
+	var text, i;
+
+	songsList = songs.length;
+	text = /*"<table>"*/ " ";
+	for (i = 0; i < songsList; i++) {
+		if (i==indexR){
+			text += "<tr><td>$</td>" + "<td>" + songs[i][1] + "</td><tr>";
+		}
+	    else {
+	    	text += "<tr><td></td>" + "<td>" + songs[i][1] + "</td><tr>";
+	    }
+	}
+	/*text += "</table>";*/
+	document.getElementById("playlistTable").innerHTML = text;
+}
+
 function playAudio() { 
     player.play(); 
     play.style.display = "none";
@@ -43,7 +76,23 @@ function pauseAudio() {
     play.style.display = "initial";
 }
 
+function playNext(){
+	if (indexR<songs.length-1){
+		indexR=indexR+1;
+		player.src = songs[indexR][0];
+		playAudio();
+	}
+	showPlaylist();
+}
 
+function playPrevious(){
+	if (indexR>0){
+		indexR=indexR-1;
+		player.src = songs[indexR][0];
+		playAudio();
+	}
+	showPlaylist();
+}
 
 //---------time slider------------
 output.innerHTML = slider.value; // Display the default slider value
@@ -76,6 +125,21 @@ player.ontimeupdate = function() {
 	output.innerHTML = currMin + ":" + currSec;
 	remainingOut.innerHTML = "Remaining: -" + remainingMin + ":" + remainingSec;
 	slider.value = currTime;
+
+	//------------
+	if (indexR==songs.length-1){
+		next.disabled = true;
+	}
+	else {
+		next.disabled = false;
+	}
+
+	if (indexR==0){
+		previous.disabled = true;
+	}
+	else{
+		previous.disabled = false;
+	}
 }
 
 // Update the current slider value (each time you drag the slider handle)
